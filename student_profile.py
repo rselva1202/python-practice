b7b24b8b2396fd3000fe7c11c46159ef6fbe2865
SELVA GANAPATHY R <rselva1204@gmail.com>
2026-06-26 18:18:47 +0530

    Create student_profile.py

---
+++ student_profile.py
@@ -0,0 +1,8 @@
+name = input("Enter Your Name:")
+age = int(input("Enter Your Age:")
+district = input("Enter Your District:")
+
+print("\n---PROFILE---")
+print("NAME:", name)
+print("AGE:", age)
+print("DISTRICT:", district)
